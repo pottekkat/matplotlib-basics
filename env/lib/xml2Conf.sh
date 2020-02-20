87ff8a9f9c3adc1c36898b65edf728b5eba1d454
Navendu Pottekkat <navendupottekkat@gmail.com>
2020-02-20 22:21:23 +0530

    initial commit

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/navendu/ML_Projects/data-science-test/env/lib"
+XML2_LIBS="-lxml2 -L/home/navendu/ML_Projects/data-science-test/env/lib -lz -L/home/navendu/ML_Projects/data-science-test/env/lib -llzma -lpthread -L/home/navendu/ML_Projects/data-science-test/env/lib  -lm "
+XML2_INCLUDEDIR="-I/home/navendu/ML_Projects/data-science-test/env/include/libxml2 -I/home/navendu/ML_Projects/data-science-test/env/include"
+MODULE_VERSION="xml2-2.9.9"
+
